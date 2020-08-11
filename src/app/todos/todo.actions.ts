8bccc67709458db512a988c850c092942cf8f357
ricardo <ricardolopez28@gmail.com>
2020-08-10 20:44:11 -0500

    toda la estructura e instalacion de NgRx

---
+++ src/app/todos/todo.actions.ts
@@ -0,0 +1,6 @@
+import { createAction, props } from '@ngrx/store';
+
+export const crear = createAction(
+    '[TODO] crea todo',
+    props<{texto: string}>()
+    );
---
+++ src/app/todos/todo.reducer.ts
@@ -0,0 +1,13 @@
+import { createReducer, on } from '@ngrx/store';
+import { crear } from './todo.actions';
+import { Todo } from './models/todo'
+
+export const initialState = Todo[] = [];
+
+const _todoReducer = createReducer(initialState,
+  on(crear, (state, { texto }) => [...state, new Todo( texto )]),
+);
+
+export function todoReducer(state, action) {
+  return _todoReducer(state, action);
+} 

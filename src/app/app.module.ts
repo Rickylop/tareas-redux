2ab3e45e22524535f65d9ad4f52b26ec36d4ab9b
ricardo <ricardolopez28@gmail.com>
2020-08-11 18:28:26 -0500

    Completando estructura y formulario reactivo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule } from '@angular/forms'
+
 //NgRx
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { todoReducer } from './todos/todo.reducer';
 // import { counterReducer } from './counter.reducer';
 
 import { AppComponent } from './app.component';
 import { TodoModule } from './todos/todo.module';
 import { FooterComponent } from './footer/footer.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -16,8 +20,10 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent,
   ],
   imports: [
-    BrowserModule, TodoModule, 
-    StoreModule.forRoot({ todos: todoReducer })
+    BrowserModule, TodoModule,
+    ReactiveFormsModule, 
+    StoreModule.forRoot({ todos: todoReducer }), 
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
     
   ],
   providers: [],

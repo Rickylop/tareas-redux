110a2653facc7e3c5c726a79f876617a2ae5106a
ricardo <ricardolopez28@gmail.com>
2020-08-11 18:29:29 -0500

    Acciones y reducer

--- src/app/todos/todo.reducer.ts
+++ src/app/todos/todo.reducer.ts
@@ -2,7 +2,9 @@ import { createReducer, on } from '@ngrx/store';
 import { crear } from './todo.actions';
 import { Todo } from './models/todo'
 
-export const initialState = Todo[] = [];
+export const initialState: Todo[] = [
+    new Todo('salvar al mundo')
+];
 
 const _todoReducer = createReducer(initialState,
   on(crear, (state, { texto }) => [...state, new Todo( texto )]),
